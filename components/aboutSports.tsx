fffdad6753088d941b0ae77b899f31adbd150df3
Subhajit Dolai <subhajitdolai999@gmail.com>
2025-05-16 21:07:25 +0530

    aboutsports card click for mobile

--- components/aboutSports.tsx
+++ components/aboutSports.tsx
@@ -1,41 +1,34 @@
-
 import Link from 'next/link'
 import Image from 'next/image'
 
 const sports = [
     {
         name: 'Swimming',
-        // role: 'Founder - CEO',
         avatar: '/sports_img/swimming.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
     {
         name: 'Badminton',
-        // role: 'Co-Founder - CTO',
         avatar: '/sports_img/badminton.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
     {
         name: 'Wrestling',
-        // role: 'Sales Manager',
         avatar: '/sports_img/wrestling.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
     {
         name: 'Table Tennis',
-        // role: 'UX Engeneer',
         avatar: '/sports_img/tableTennis.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
     {
         name: 'Foosball',
-        // role: 'Interaction Designer',
         avatar: '/sports_img/foosball.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
     {
         name: 'Chess',
-        // role: 'Visual Designer',
         avatar: '/sports_img/chess.webp',
         link: 'https://mitwpu.edu.in/life-wpu/sports',
     },
@@ -57,10 +50,9 @@ export default function AboutSports() {
                 <div className="mt-12 md:mt-24">
                     <div className="grid gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
                         {sports.map((sport, index) => (
-                            <div key={index} className="group overflow-hidden">
-                                {/* <img className="h-96 w-full rounded-md object-cover object-top grayscale transition-all duration-500 hover:grayscale-0 group-hover:h-[22.5rem] group-hover:rounded-xl" src={sport.avatar} alt="team sport" width="826" height="1239" /> */}
+                            <div key={index} className="group overflow-hidden focus:outline-none" tabIndex={0}>
                                 <Image
-                                    className="h-96 w-full rounded-md object-cover object-top grayscale transition-all duration-500 hover:grayscale-0 group-hover:h-[22.5rem] group-hover:rounded-xl"
+                                    className="h-96 w-full rounded-md object-cover object-top grayscale transition-all duration-500 hover:grayscale-0 group-hover:h-[22.5rem] group-hover:rounded-xl group-focus:grayscale-0 group-focus:h-[22.5rem] group-focus:rounded-xl"
                                     src={sport.avatar}
                                     alt="team sport"
                                     width={826}
@@ -68,20 +60,12 @@ export default function AboutSports() {
                                 />
                                 <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
                                     <div className="flex justify-between">
-                                        <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wider">{sport.name}</h3>
-                                        {/* <span className="text-xs">_0{index + 1}</span> */}
-                                        <Link href={sport.link} className="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100">
+                                        <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wider group-focus:tracking-wider">{sport.name}</h3>
+                                        <Link href={sport.link} className="group-hover:text-primary-600 group-focus:text-primary-600 dark:group-hover:text-primary-400 dark:group-focus:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100 group-focus:translate-y-0 group-focus:opacity-100">
                                             {' '}
                                             Learn more
                                         </Link>
                                     </div>
-                                    {/* <div className="mt-1 flex items-center justify-between">
-                                        <span className="text-muted-foreground inline-block translate-y-6 text-sm opacity-0 transition duration-300 group-hover:translate-y-0 group-hover:opacity-100">{sport.role}</span>
-                                        <Link href={sport.link} className="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100">
-                                            {' '}
-                                            Learn more
-                                        </Link>
-                                    </div> */}
                                 </div>
                             </div>
                         ))}
